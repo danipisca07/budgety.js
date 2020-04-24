eeac36c523fc2dd708612362ac30de514a4bbbef
Daniele Piscaglia <danielepiscaglia@libero.it>
2020-04-25 00:02:23 +0200

    Aggiunta eventi

--- app.js
+++ app.js
@@ -7,5 +7,23 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function() {
+        //Ottieni input
+        //Aggiungi al budget controller
+        //Aggiungi alla UI
+        //Calcola budget
+        //Mostra il budget
+    }
+
+    //Aggiunta voce di budget
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    
+    document.addEventListener("keypress", function(event){
+        if(event.keyCode === 13 || event.which === 13){ //Enter
+            ctrlAddItem();
+        }
+    });
+
     return {}
 })(budgetController, UIController);

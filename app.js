f3564744c697ed0c2e1d9e8d8f80f1df75a3bedb
Daniele Piscaglia <danielepiscaglia@libero.it>
2020-04-24 23:52:18 +0200

    Specifica moduli

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetController = (function() {
+    return {}
+})();
+
+var UIController = (function() {
+    return {}
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    return {}
+})(budgetController, UIController);

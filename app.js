683fcd068e971177b9ef563c3176572c1f070c3c
Daniele Piscaglia <danielepiscaglia@libero.it>
2020-04-25 17:23:22 +0200

    Aggiunta funzioni a prototype

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
     };
+
+    Expense.prototype.calcPercentage = function(totalIncome){
+        if(totalIncome>0)
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        else
+            this.percentage = -1;
+    };
+
+    Expense.prototype.getPercentage = function(){ return this.percentage; };
 
     var Income = function(id, description, value) {
         this.id = id;
